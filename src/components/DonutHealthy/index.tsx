e21338d9e33c250f4190648f59ede3a4d42f9984
smakosh <ismai23l@hotmail.com>
2020-11-08 02:48:59 +0100

    fix: handle apex chart

--- src/components/DonutHealthy/index.tsx
+++ src/components/DonutHealthy/index.tsx
@@ -1,44 +1,42 @@
-import React, { useState } from 'react';
-
-import ApexCharts from 'apexcharts'
-
-import { Container } from './styles';
+import dynamic from "next/dynamic";
+import { Container } from "./styles";
 import { FiCircle, FiCheckCircle } from "react-icons/fi";
 
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+	ssr: false,
+});
 
-const DonutHealthy: React.FC = () => {
-    const [options, setOptions] = useState({
-        series: [70],
-        options: {
-            chart: {
-                height: 350,
-                type: 'radialBar',
-            },
-            plotOptions: {
-                radialBar: {
-                    hollow: {
-                        size: '70%',
-                    }
-                },
-            },
-            labels: ['Cricket'],
-        },
-    });
+const DonutHealthy = () => {
+	const options = {
+		series: [70],
+		options: {
+			chart: {
+				height: 350,
+				type: "radialBar",
+			},
+			plotOptions: {
+				radialBar: {
+					hollow: {
+						size: "70%",
+					},
+				},
+			},
+			labels: ["Cricket"],
+		},
+	};
 
-    // useEffect(() => {
-
-    // }, []);
-
-    return (
-
-        <Container>
-            <p>Walking</p>
-            {/* <ApexCharts></ApexCharts> */}
-            <div id="chart">
-                <ReactApexChart options={options.options} series={options.series} type="radialBar" height={350} />
-            </div>
-
-        </Container>
-    )
-}
+	return (
+		<Container>
+			<p>Walking</p>
+			<div id="chart">
+				<ReactApexChart
+					options={options.options}
+					series={options.series}
+					type="radialBar"
+					height={350}
+				/>
+			</div>
+		</Container>
+	);
+};
 export default DonutHealthy;
